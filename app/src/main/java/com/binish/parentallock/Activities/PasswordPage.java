18a8f6be2409294db3c169fb806e547dc93355ab
Binish Manandhar <david.punkie32@gmail.com>
2019-06-21 12:47:47 +0545

    Updates on UI / UX on the Password Page

--- app/src/main/java/com/binish/parentallock/Activities/PasswordPage.java
+++ app/src/main/java/com/binish/parentallock/Activities/PasswordPage.java
@@ -1,18 +1,24 @@
 package com.binish.parentallock.Activities;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.andrognito.patternlockview.PatternLockView;
 import com.andrognito.patternlockview.listener.PatternLockViewListener;
@@ -32,12 +38,12 @@ public class PasswordPage extends AppCompatActivity implements OnClickListener {
     ImageView passwordSet;
     NumberPicker passwordType;
     CheckBox checkBox;
-    CardView passwordPattern;
+    Button passwordPattern;
     CardView passwordText;
     DatabaseHelper databaseHelper;
     PatternLockView patternLockView;
     TextView patternDescription;
-    String nPattern="";
+    String nPattern = "";
     String cPattern;
 
     @Override
@@ -54,14 +60,14 @@ public class PasswordPage extends AppCompatActivity implements OnClickListener {
         confirmPassword = findViewById(R.id.confirm_password);
         passwordSet = findViewById(R.id.password_set);
         checkBox = findViewById(R.id.default_fingerprint);
-        passwordPattern = findViewById(R.id.password_pattern_cardView);
+        passwordPattern = findViewById(R.id.pattern_button);
         passwordText = findViewById(R.id.password_text_cardView);
-        patternLockView = findViewById(R.id.password_page_pattern);
-        patternDescription = findViewById(R.id.pattern_description);
+        final HorizontalScrollView hsv = findViewById(R.id.password_horizontalScroll);
+
 
         if (UsefulFunctions.checkUniversalPasswordExist(this))
             passwordSet.setImageResource(R.drawable.ic_done_green_24dp);
-        if(!BiometricUtils.isHardwareSupported(this)) {
+        if (!BiometricUtils.isHardwareSupported(this)) {
             CardView fingerprintEnabled = findViewById(R.id.fingerprint_enabled);
             fingerprintEnabled.setVisibility(View.GONE);
         }
@@ -72,33 +78,43 @@ public class PasswordPage extends AppCompatActivity implements OnClickListener {
 
         passwordType.setMinValue(0);
         passwordType.setMaxValue(1);
-        passwordType.setDisplayedValues(new String[]{"Text","Pattern"});
-        if(databaseHelper.getUniversalPasswordType().equals(GlobalStaticVariables.PASSWORDTYPE_PATTERN)) {
+        passwordType.setDisplayedValues(new String[]{"Text", "Pattern"});
+        if (databaseHelper.getUniversalPasswordType().equals(GlobalStaticVariables.PASSWORDTYPE_PATTERN)) {
             passwordType.setValue(1);
-            passwordText.setVisibility(View.GONE);
+            delayScrolling(hsv);
         }
-        else
-            passwordPattern.setVisibility(View.GONE);
+
+        hsv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
 
         passwordType.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                if(newVal==1) {
-                    passwordText.setVisibility(View.GONE);
-                    passwordPattern.setVisibility(View.VISIBLE);
-                }else {
-                    passwordText.setVisibility(View.VISIBLE);
-                    passwordPattern.setVisibility(View.GONE);
+                if (newVal == 1) {
+                    hsv.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+                    newPassword.setEnabled(false);
+                    confirmPassword.setEnabled(false);
+                } else {
+                    hsv.fullScroll(HorizontalScrollView.FOCUS_LEFT);
+                    newPassword.setEnabled(true);
+                    confirmPassword.setEnabled(true);
                 }
             }
         });
 
+        passwordPattern.setOnClickListener(this);
+    }
 
+    private void patternLockWork() {
         patternLockView.setTactileFeedbackEnabled(true);
         patternLockView.addPatternLockListener(new PatternLockViewListener() {
             @Override
             public void onStarted() {
-                if(nPattern.equals(""))
+                if (nPattern.equals(""))
                     patternDescription.setText("New Pattern");
             }
 
@@ -109,24 +125,22 @@ public class PasswordPage extends AppCompatActivity implements OnClickListener {
 
             @Override
             public void onComplete(List<PatternLockView.Dot> pattern) {
-                if(nPattern.equals("")) {
+                if (nPattern.equals("")) {
                     nPattern = PatternLockUtils.patternToMD5(patternLockView, pattern);
                     patternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT);
                     patternLockView.clearPattern();
                     patternDescription.setText("Confirm Pattern");
-                }
-                else {
+                } else {
                     cPattern = PatternLockUtils.patternToMD5(patternLockView, pattern);
-                    if(nPattern.equals(cPattern)){
-                        databaseHelper.insertPassword(nPattern,checkBox.isChecked(),passwordType.getValue());
+                    if (nPattern.equals(cPattern)) {
+                        databaseHelper.insertPassword(nPattern, checkBox.isChecked(), passwordType.getValue());
                         patternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT);
                         delayMovingBack();
                         patternDescription.setText("Pattern Confirmed");
-                    }
-                    else {
+                    } else {
                         patternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
                         patternDescription.setText("Incorrect! Try again");
-                        nPattern="";
+                        nPattern = "";
                     }
                 }
             }
@@ -154,7 +168,7 @@ public class PasswordPage extends AppCompatActivity implements OnClickListener {
                     passwordSet.setImageResource(R.drawable.ic_done_green_24dp);
                     Snackbar.make(v, "Password Stored:", Snackbar.LENGTH_SHORT).show();
 
-                    databaseHelper.insertPassword(nPassword,checkBox.isChecked(),passwordType.getValue());
+                    databaseHelper.insertPassword(nPassword, checkBox.isChecked(), passwordType.getValue());
                     delayMovingBack();
                 } else {
                     Snackbar.make(v, "Passwords don't match", Snackbar.LENGTH_SHORT).show();
@@ -169,6 +183,15 @@ public class PasswordPage extends AppCompatActivity implements OnClickListener {
                 break;
             case R.id.password_pattern_back:
                 onBackPressed();
+                break;
+            case R.id.pattern_button:
+                View view = LayoutInflater.from(PasswordPage.this).inflate(R.layout.dialog_pattern_lock, null);
+                Dialog dialog = new Dialog(PasswordPage.this);
+                patternLockView = view.findViewById(R.id.password_page_pattern);
+                patternDescription = view.findViewById(R.id.pattern_description);
+                dialog.setContentView(view);
+                dialog.show();
+                patternLockWork();
                 break;
         }
     }
@@ -197,6 +220,22 @@ public class PasswordPage extends AppCompatActivity implements OnClickListener {
             @Override
             public void onFinish() {
                 onBackPressed();
+            }
+        }.start();
+    }
+
+    private void delayScrolling(final HorizontalScrollView hsv){
+        new CountDownTimer(800,1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                hsv.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+                newPassword.setEnabled(false);
+                confirmPassword.setEnabled(false);
             }
         }.start();
     }
